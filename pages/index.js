dc5a4c0a6ed4b64c2082b81c92131d13da31e2b0
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-12-22 22:20:16 +0900

    modified:   .github/workflows/main.yml modified:   package.json modified:   pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
 export default function Home() {
-  
+    return(<>
+        <div>
+            <h1>Base64 to image api</h1>
+            <div>example request:</div>
+            <div>
+                <a href="./api/image?base64=iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAApklEQVR4Xm2MvQ3CMBCFbx9vkYo1kiYxCkaJoxikmCJrREgMEv7EAAxARUnryt3jzlKoKD7d8%2FvORzFGKKXgvceBWaZ04mgcR%2FR9j71zqRR2nB0jjvwwoG1bNE2Dzlp0nJe3XCPZtnWNTVkmlixTHFneNlyssgxVUfyyTLlIW2OgtcbpcQM b1R5juk6p04crTmEEHC8n4HXMzFd5tSJo1Tyz7 w wITQKlwkgKcqAAAAABJRU5ErkJggg%3D%3D">
+                    test link
+                </a>
+            </div>
+        </div>
+    </>);
 }
