3698a59f1816a8c82aef9aed044f9239a1a15413
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-12-22 22:33:17 +0900

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: process.env.GITHUB_ACTIONS && "/Base64toURL",
   reactStrictMode: true,
+  trailingSlash: true
 }
 
 module.exports = nextConfig
