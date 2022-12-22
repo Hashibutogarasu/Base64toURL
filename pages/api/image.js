0f0b6d49aef72cded0b9d16cef242f7b264d8488
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-12-22 18:35:50 +0900

    modified:   pages/api/image.js

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,14 +1,21 @@
 var url = require('url');
+import { NextApiRequest, NextApiResponse } from 'next';
 
+/**
+ * 
+ * @param {NextApiRequest} req 
+ * @param {NextApiResponse} res 
+ */
 export default function handler(req, res) {
     let b64 = encodeURI(req.query.base64).split("%20").join("+");
     const buffer = Buffer.from(b64,"base64");
-    
+
     res.setHeader('Content-Type', "image/png");
     res.setHeader("Content-Length", buffer.length);
-    res.write(buffer);
+    // res.write(buffer);
+    
+    res.status(200).write(buffer);
     res.end(buffer, "binary");
-    console.log(b64);
 
     // return { props: {}}
 }
