2735f1fd61067ac02ff6b971e0ed4b63bf1fc1d9
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-12-22 17:26:41 +0900

    modified:   next.config.js new file:   pages/api/image.js modified:   pages/index.js

---
+++ pages/api/image.js
@@ -0,0 +1,14 @@
+var url = require('url');
+
+export default function handler(req, res) {
+    let b64 = encodeURI(req.query.base64).split("%20").join("+");
+    const buffer = Buffer.from(b64,"base64");
+    
+    res.setHeader('Content-Type', "image/png");
+    res.setHeader("Content-Length", buffer.length);
+    res.write(buffer);
+    res.end(buffer, "binary");
+    console.log(b64);
+
+    // return { props: {}}
+}
