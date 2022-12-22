f4065c9e164a2884bdceb259c16812cc1827d51e
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2022-12-22 22:39:28 +0900

    modified:   .github/workflows/main.yml modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: process.env.GITHUB_ACTIONS && "/Base64toURL",
+  basePath: process.env.GITHUB_ACTIONS ? "/Base64toURL" : "",
   reactStrictMode: true,
   trailingSlash: true
 }
